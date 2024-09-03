2968a2a77ce8f1c95d4d44b13515b0002ed70132
fullstackmechanic <sirhmvfx@gmail.com>
2024-09-03 20:01:50 +0100

    wokring on auth

---
+++ src/app/login/page.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+function Login() {
+  return (
+    <>
+      <div>
+        <div className="bg-[#E4FFF3] w-2/4 h-screen flex flex-col justify-between">
+          <div className="px-16 pt-16 flex flex-col gap-12">
+            <div className="w-[150px]">
+              <Image
+                src={"/logo.png"}
+                width={1000}
+                height={1000}
+                alt="logo"
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            <div>
+              <h1 className="font-medium text-4xl w-4/6">
+                Welcome Back to GreenRoyale{" "}
+              </h1>
+              <p className="text-xl w-4/6 ">
+                Discover cutting-edge recycling solution powered by AI
+              </p>
+            </div>
+          </div>{" "}
+          <div className="px-20 pt-10">
+            <Image
+              src={"/loginimage.png"}
+              width={1000}
+              height={1000}
+              className="w-full h-full object-cover"
+              alt="img"
+            />
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
